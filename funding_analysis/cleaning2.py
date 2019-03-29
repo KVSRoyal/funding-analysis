57b3e60b838e375757280db9019b46e35cf7fbeb
sarahsul <48837374+sarahsul@users.noreply.github.com>
2019-03-29 13:01:04 -0600

    Added Comments to Cleaning

--- funding_analysis/cleaning2.py
+++ funding_analysis/cleaning2.py
@@ -7,12 +7,14 @@ import xlrd
 import matplotlib
 import matplotlib.pyplot as pp
 
+#path is wrong now
 demographic_df = pd.read_excel('datademo.xls')
 
-#Capitalize all names of STATES
+#Capitalize all names of STATES - not sure if this works
 demographic_df['STATE'] = map(lambda x: x.upper(), demographic_df['STATE'])
 
 #Delete rows without relevant information
 demographic_df = demographic_df[demographic_df.TOTAL != 0]
 
-ExcelFile.parse(demographic_df)
+#have not tested this
+demographic_df.to_excel(output.xlsx)
